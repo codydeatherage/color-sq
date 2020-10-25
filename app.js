480831fb580981bb3b0e1d9fb952c199b585a2c7
codydeatherage <cody@DESKTOP-DCKI10R.localdomain>
2020-10-25 04:11:07 -0700

    locked out inputs during sequencing

--- app.js
+++ app.js
@@ -9,13 +9,12 @@ class Game{
         this.sections = [this.topL, this.topR, this.botL, this.botR];
         this.starter = starter;
         this.sequence = [];
-        this.inputLocked = true;
-
         this.starter.addEventListener('click', async ()=>{
             await this.startUp();
-        })
+        });
     }
     startSequence = async (n) => {
+        this.lockInputs(); console.log('input locked...');
         for(let i = 0; i < n; i++){
             let index = Math.floor(Math.random() * 4);
             let nextLight = this.sections[index].color;
@@ -36,8 +35,10 @@ class Game{
             await this.timer(500);
             classList.remove(classToAdd);
         }
+        this.unlockInputs(); console.log('input unlocked...');
     }
     startUp = async() =>{
+        this.lockInputs(); console.log('input locked...');
         for(let i = 0; i < 4; i++){
             let color = this.sections[i].color;
             for(let j = 0; j < 4; j++){ 
@@ -59,17 +60,34 @@ class Game{
             }
         }
         await this.startSequence(3);
+        this.unlockInputs();console.log('input unlocked...');
     }
     timer = async (ms)=>{
         return new Promise(res => setTimeout(res, ms));
     }
+    lockInputs = async () =>{
+        for(let s of this.sections){
+            console.log(s);
+            s.inputLocked = true;
+        }
+    }
+    unlockInputs = async () =>{
+        for(let s of this.sections){
+            s.inputLocked = false;
+        }
+    }
 }
 
 class GameSection{
-    constructor(el, color){
+    constructor(el, color, inputLocked){
         this.el = el;
         this.color = color;
-        this.el.addEventListener('click', this.buttonFlash.bind(null, this.el, this.color), false);
+        this.inputLocked = inputLocked;
+        this.el.addEventListener('click', ()=>{
+            if(!this.inputLocked){
+                this.buttonFlash(this.el, this.color);
+            } 
+        });
     }
     buttonFlash = (el) =>{
         let newClass = 'lightup-'+ this.color;
@@ -87,10 +105,10 @@ const top_left = document.getElementById('top-left');
 const bot_left = document.getElementById('bot-left');
 const top_right = document.getElementById('top-right');
 const bot_right = document.getElementById('bot-right');
-let topLeft = new GameSection(top_left, 'yellow');
-let topRight = new GameSection(top_right, 'blue');
-let botLeft = new GameSection(bot_left, 'red');
-let botRight = new GameSection(bot_right, 'green');
+let topLeft = new GameSection(top_left, 'yellow', false);
+let topRight = new GameSection(top_right, 'blue', false);
+let botLeft = new GameSection(bot_left, 'red', false);
+let botRight = new GameSection(bot_right, 'green', false);
 
 let game = new Game(play_btn, topLeft, topRight, botLeft, botRight);
 
